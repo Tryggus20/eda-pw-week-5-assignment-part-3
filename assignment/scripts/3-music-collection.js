15ec9da2bb63160aa4c1e765039ec8cae2054a4e
David Campbell <tryggus20@gmail.com>
2023-08-16 11:29:04 -0500

    added 5 albums and started on showCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -11,4 +11,15 @@ function addToCollection(title, artist, yearPublished) {
     return album;
 }// end of addToCollection
 
+//adding albums:
+addToCollection("Blaine's Greatest Hits", 'Blaine Booher', 2023 );
+
 addToCollection('Hybrid Theory', 'Linkin Park', 2000 );
+addToCollection(`That's the Spirit`, 'Bring Me the Horizon', 2015 );
+addToCollection('Ember', 'Breaking Benjamin', 2018 );
+addToCollection('The Stories We Tell Ourselves', 'Nothing More', 2017 );
+addToCollection('The Sickness', 'Disturbed', 2000 );
+
+function showCollection() {
+    
+}// end of showCollection
